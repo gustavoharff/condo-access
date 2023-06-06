fb04ac34fca7f3a9fa915dd67fb9e737162d65b8
Gustavo Harff <gustavo.harff@gmail.com>
2023-06-05 21:18:54 -0300

    feat: Add tabs navigation

--- app/home/_layout.tsx
+++ app/home/_layout.tsx
@@ -1,7 +1,8 @@
-import { Stack, Slot } from "expo-router";
+import { Stack, Tabs } from "expo-router";
 import { Provider } from "../../src/context/auth";
 import { Logo } from "../../src/components/logo";
-import { gray } from "tailwindcss/colors";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { red } from "tailwindcss/colors";
 
 export const unstable_settings = {
   initialRouteName: "index",
@@ -11,18 +12,55 @@ export default function Root() {
   return (
     // Setup the auth context and render our layout inside of it.
     <Provider>
-      <Stack>
-        <Stack.Screen
+      <Tabs
+        sceneContainerStyle={{
+          backgroundColor: "transparent",
+        }}
+        screenOptions={{
+          tabBarStyle: {
+            backgroundColor: "#1E2225",
+          },
+          tabBarActiveTintColor: red[500]
+        }}
+      >
+        <Tabs.Screen
           name="index"
           options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name={"car-clock"}
+                size={size}
+                color={color}
+              />
+            ),
             headerTitle: () => <Logo size={28} />,
             headerStyle: {
-              backgroundColor: '#1E2225',
+              backgroundColor: "#1E2225",
             },
-            contentStyle: { backgroundColor: "transparent" },
+            href: "/home",
           }}
         />
-      </Stack>
+
+        <Tabs.Screen
+          name="cars"
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name={"car"}
+                size={size}
+                color={color}
+              />
+            ),
+            headerTitle: () => <Logo size={28} />,
+            headerStyle: {
+              backgroundColor: "#1E2225",
+            },
+            href: "/home/cars",
+          }}
+        />
+      </Tabs>
     </Provider>
   );
 }
